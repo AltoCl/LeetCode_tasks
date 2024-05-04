959bf764e7a858acf7d3f67d180b7ca7df2bc05a
AltoCl <AltoC@protonmail.com>
2024-05-04 13:00:30 +0300

    4.05

---
+++ May24/881. Boats to Save People.py
@@ -0,0 +1,17 @@
+# You are given an array people where people[i] is the weight of the ith person,
+# and an infinite number of boats where each boat can carry a maximum weight of limit.
+# Each boat carries at most two people at the same time, provided the sum of the weight of those people is at most limit.
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        left, right = 0, len(people) - 1
+        boats = 0
+
+        while left <= right:
+            if people[left] + people[right] <= limit:
+                left += 1
+            right -= 1
+            boats += 1
+
+        return boats
