ec2e35df3d9a230a434afacf9a96a3cf48963d95
AltoCl <AltoC@protonmail.com>
2024-08-25 13:25:08 +0300

    25.08

--- August24/145. Binary Tree Postorder Traversal.py
+++ August24/145. Binary Tree Postorder Traversal.py
@@ -1,6 +1,3 @@
-# Given the root of a binary tree, return the postorder traversal of its nodes' values.
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
