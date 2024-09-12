cabd525e0f87fe2bfa6b2e9dacc0feda07ac619f
AltoCl <AltoC@protonmail.com>
2024-09-12 13:56:46 +0300

    12.09

---
+++ September24/1684. Count the Number of Consistent Strings.py
@@ -0,0 +1,14 @@
+# You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+#
+# Return the number of consistent strings in the array words.
+
+class Solution:
+    def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
+        allowed_set = set(allowed)  # Convert allowed string to a set for O(1) lookups
+        consistent_count = 0
+
+        for word in words:
+            if all(char in allowed_set for char in word):
+                consistent_count += 1
+
+        return consistent_count
