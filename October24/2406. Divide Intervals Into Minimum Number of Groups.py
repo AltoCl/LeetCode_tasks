471c478bc3e59e3847d35fa4ac3b241e61654e60
AltoCl <AltoC@protonmail.com>
2024-10-12 13:30:56 +0300

    12.10

--- October24/2406. Divide Intervals Into Minimum Number of Groups.py
+++ October24/2406. Divide Intervals Into Minimum Number of Groups.py
@@ -5,3 +5,17 @@
 # Return the minimum number of groups you need to make.
 #
 # Two intervals intersect if there is at least one common number between them. For example, the intervals [1, 5] and [5, 8] intersect.
+
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        start_times = sorted(i[0] for i in intervals)
+        end_times = sorted(i[1] for i in intervals)
+        end_ptr, group_count = 0, 0
+
+        for start in start_times:
+            if start > end_times[end_ptr]:
+                end_ptr += 1
+            else:
+                group_count += 1
+
+        return group_count
