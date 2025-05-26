deefcb22d1fd88384bb410f1310648f792329584
AltoCl <AltoC@protonmail.com>
2025-05-26 11:56:55 +0300

    26.05

---
+++ may25/1857. Largest Color Value in a Directed Graph.py
@@ -0,0 +1,37 @@
+# There is a directed graph of n colored nodes and m edges. The nodes are numbered from 0 to n - 1.
+#
+# You are given a string colors where colors[i] is a lowercase English letter representing the color of the ith node in this graph (0-indexed). You are also given a 2D array edges where edges[j] = [aj, bj] indicates that there is a directed edge from node aj to node bj.
+#
+# A valid path in the graph is a sequence of nodes x1 -> x2 -> x3 -> ... -> xk such that there is a directed edge from xi to xi+1 for every 1 <= i < k. The color value of the path is the number of nodes that are colored the most frequently occurring color along that path.
+#
+# Return the largest color value of any valid path in the given graph, or -1 if the graph contains a cycle.
+
+
+class Solution:
+    def largestPathValue(self, colors: str, edges: List[List[int]]) -> int:
+        hashmap = collections.defaultdict(list)
+
+        for u, v in edges:
+            if u == v:
+                return -1
+            hashmap[u].append(v)
+
+        visited = set()
+
+        @cache
+        def dfs(curr, target):
+            result = 0
+            for adj in hashmap[curr]:
+                if adj in visited:
+                    return float('inf')
+                visited.add(adj)
+                result = max(result, dfs(adj, target))
+                visited.remove(adj)
+
+            return result + 1 if colors[curr] == target else result
+
+        max_path_value = 0
+        for i in range(len(colors)):
+            max_path_value = max(max_path_value, dfs(i, colors[i]))
+
+        return -1 if max_path_value == float('inf') else max_path_value
