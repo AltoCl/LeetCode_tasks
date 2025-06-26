02e3f07ad3ab7a2be62834be9cbfb013dc228955
AltoCl <AltoC@protonmail.com>
2025-06-26 13:21:37 +0300

    26.06

--- June25/2311. Longest Binary Subsequence Less Than or Equal to K.py
+++ June25/2311. Longest Binary Subsequence Less Than or Equal to K.py
@@ -10,5 +10,21 @@
 
 
 class Solution:
-    def numberOfArrays(self, diff: List[int], lower: int, upper: int) -> int:
-        return (K := list(accumulate(diff, initial=0))) and max(0, upper - lower + 1 - max(K) + min(K))
+    def longestSubsequence(self, s: str, k: int) -> int:
+        n = len(s)
+        zeros = s.count('0')
+        ones = 0
+        value = 0
+        power = 1
+
+        for i in range(n - 1, -1, -1):
+            if s[i] == '1':
+                if value + power > k:
+                    continue
+                value += power
+                ones += 1
+            power <<= 1
+            if power > k:
+                break
+
+        return zeros + ones
