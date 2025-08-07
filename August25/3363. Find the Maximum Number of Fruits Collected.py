a3b493715797bc42504739ad749f14cd19dfd183
AltoCl <AltoC@protonmail.com>
2025-08-07 18:05:13 +0300

    7.08

--- August25/3363. Find the Maximum Number of Fruits Collected.py
+++ August25/3363. Find the Maximum Number of Fruits Collected.py
@@ -1,43 +1,40 @@
-# There is a game dungeon comprised of n x n rooms arranged in a grid.
-# #
-# # You are given a 2D array fruits of size n x n, where fruits[i][j] represents the number of fruits in the room (i, j). Three children will play in the game dungeon, with initial positions at the corner rooms (0, 0), (0, n - 1), and (n - 1, 0).
-# #
-# # The children will make exactly n - 1 moves according to the following rules to reach the room (n - 1, n - 1):
-# #
-# # The child starting from (0, 0) must move from their current room (i, j) to one of the rooms (i + 1, j + 1), (i + 1, j), and (i, j + 1) if the target room exists.
-# # The child starting from (0, n - 1) must move from their current room (i, j) to one of the rooms (i + 1, j - 1), (i + 1, j), and (i + 1, j + 1) if the target room exists.
-# # The child starting from (n - 1, 0) must move from their current room (i, j) to one of the rooms (i - 1, j + 1), (i, j + 1), and (i + 1, j + 1) if the target room exists.
-# # When a child enters a room, they will collect all the fruits there. If two or more children enter the same room, only one child will collect the fruits, and the room will be emptied after they leave.
-# #
-# # Return the maximum number of fruits the children can collect from the dungeon.
-
 class Solution:
     def maxCollectedFruits(self, mat: List[List[int]]) -> int:
         self.n = len(mat)
+        self.memo = [[-1] * self.n for _ in range(self.n)]
 
-        # row = i, col = j
         def dfs3(row, col):
             if row < 0 or col < 0 or row >= self.n or col >= self.n:
                 return 0
 
+            if self.memo[row][col] != -1:
+                return self.memo[row][col]
+
             val = mat[row][col]
             res = 0
 
+            # row = i, col = j
             if row == col:
                 res = max(res, dfs3(row + 1, col + 1))
             elif row - 1 == col:
-                res = max(res, dfs3(row + 1, col + 1))
-                res = max(res, dfs3(row, col + 1))
+                res = max(res,
+                          dfs3(row + 1, col + 1),
+                          dfs3(row, col + 1))
             else:
-                res = max(res, dfs3(row + 1, col + 1))
-                res = max(res, dfs3(row, col + 1))
-                res = max(res, dfs3(row - 1, col + 1))
+                res = max(res,
+                          dfs3(row + 1, col + 1),
+                          dfs3(row, col + 1),
+                          dfs3(row - 1, col + 1))
 
-            return val + res
+            self.memo[row][col] = val + res
+            return self.memo[row][col]
 
         def dfs2(row, col):
             if row < 0 or col < 0 or row >= self.n or col >= self.n:
                 return 0
+
+            if self.memo[row][col] != -1:
+                return self.memo[row][col]
 
             val = mat[row][col]
             res = 0
@@ -45,14 +42,17 @@ class Solution:
             if row == col:
                 res = max(res, dfs2(row + 1, col + 1))
             elif row == col - 1:
-                res = max(res, dfs2(row + 1, col + 1))
-                res = max(res, dfs2(row + 1, col))
+                res = max(res,
+                          dfs2(row + 1, col + 1),
+                          dfs2(row + 1, col))
             else:
-                res = max(res, dfs2(row + 1, col + 1))
-                res = max(res, dfs2(row + 1, col))
-                res = max(res, dfs2(row + 1, col - 1))
+                res = max(res,
+                          dfs2(row + 1, col + 1),
+                          dfs2(row + 1, col),
+                          dfs2(row + 1, col - 1))
 
-            return val + res
+            self.memo[row][col] = val + res
+            return self.memo[row][col]
 
         total = 0
 
